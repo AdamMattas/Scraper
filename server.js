bd2e1ff0c5d4165c7153763c5482938f69e64691
adam fader <adamfader@gmail.com>
2016-09-10 22:47:55 -0400

    changed port connection

--- server.js
+++ server.js
@@ -275,6 +275,9 @@ app.get('/note/manual', function(req, res) {
 });
 
 // listen on port 3000
-app.listen(3000, function() {
-  console.log('App running on port 3000!');
-});
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+// app.listen(3000, function() {
+//   console.log('App running on port 3000!');
+// });
